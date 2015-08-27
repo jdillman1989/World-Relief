8d86bcbf0b3e634befda8d5fc34c88fbc4cc4fc3
Jesse Dillman <JDillman@bibleleague.org>
2015-08-27 15:57:49 -0500

    initial commit

---
+++ includes/footer.inc.php
@@ -0,0 +1,31 @@
+<div class="footer">
+
+	<div class="social">
+
+		<ul>
+
+			<li>
+				<a href="https://www.facebook.com"></a>
+			</li>
+
+			<li>
+				<a href="https://www.twitter.com"></a>
+			</li>
+
+			<li>
+				<a href="https://instagram.com"></a>
+			</li>
+
+			<li>
+				<a href="https://www.linkedin.com"></a>
+			</li>
+		</ul>
+	</div>
+
+	<div class="copyright">
+
+		<p>&copy; World Relief. All Rights Reserved.</p>
+
+		<p>7 East Baltimore Street, Baltimore MD 21202 | 443.451.1900</p>
+	</div>
+</div>
---
+++ includes/header.inc.php
@@ -0,0 +1,12 @@
+<header class="nav animate">
+	<div class="mobile-menu animate"></div>
+	<ul id="main-nav">
+		<li><a href="#">ABOUT</a></li>
+		<li><a href="#">IMPACT</a></li>
+		<li><a href="#">JOIN</a></li>
+		<li><a href="#">CHURCH</a></li>
+		<li><a href="#">BLOG</a></li>
+		<li><a href="#">GIVE</a></li>
+		<li><a href="#">SIGN UP</a></li>
+	</ul>
+</header>
