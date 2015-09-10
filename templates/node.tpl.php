fa70fce4e4273f6125c01ab4953aa34ce8659732
Jesse Dillman <JDillman@bibleleague.org>
2015-09-10 15:26:15 -0500

    template bg img

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -1,11 +1,35 @@
 <section class="block">
 
-	<div class="wr-node node-<?php print $node->nid; ?>">
+	<?php
 
-		<?php if ($node->nid==32): ?>
-			<div class="logo"></div>
-		<?php endif; ?>
+	if ($node->field_background_image){
 
-		<?php print render($content); ?>
+		$url = file_create_url($node->field_background_image[$node->language][0]['uri']);
+
+	?>
+
+	<div class="wr-node node-<?php print $node->nid; ?>" style="background: url(<?php echo $url; ?>); background-size:cover; color:#ffffff;">
+
+	<?php 
+
+	}
+
+	else{
+
+	?>
+
+	<div class="wr-node node-<?php print $node->nid; ?>" style="color:#131313;">
+
+	<?php
+
+	}
+
+	?>
+
+	<?php if ($node->nid==32): ?>
+		<div class="logo"></div>
+	<?php endif; ?>
+
+	<?php print render($content); ?>
 	</div>
 </section>
